f7cc1552d5d3c4dc0a14a4e1b165e5c8576a7d44
alexandre xu <alexandre.xu.mail@gmail.com>
2023-03-23 22:17:59 +0100

    Add Modal1

---
+++ FrontEnd/assets/scripts/auth.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const loginForm = document.getElementById("login-form");
+
+    if (loginForm) {
+        loginForm.addEventListener("submit", (event) => {
+            event.preventDefault();
+
+            const email = document.getElementById("email").value;
+            const password = document.getElementById("password").value;
+
+            fetch("http://localhost:5678/api/users/login", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ email, password }),
+            })
+                .then((response) => {
+                    if (response.ok) {
+                        return response.json();
+                    } else {
+                        throw new Error("Erreur lors de la connexion");
+                    }
+                })
+                .then((data) => {
+                    // Stockez le token d'authentification et redirigez l'utilisateur vers la page d'édition ou la page principale
+                    localStorage.setItem("authToken", data.token);
+                    window.location.href = "index.html";
+                })
+                .catch((error) => {
+                    console.error("Erreur:", error);
+                    // Affichez un message d'erreur à l'utilisateur
+                    const errorMessageElement = document.getElementById("error-message");
+                    errorMessageElement.textContent = "Erreur lors de la connexion. Veuillez vérifier vos identifiants et réessayer.";
+                    errorMessageElement.style.display = "block";
+                });
+        });
+    }
+});
---
+++ FrontEnd/assets/scripts/script.js
@@ -0,0 +1,183 @@
+let listWorks = [];
+let WorksFiltered = [];
+let currentSelectedCategoryId = null;
+
+function fetchCategories() {
+    fetch('http://localhost:5678/api/categories')
+        .then((response) => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error('Erreur lors de la récupération des données');
+            }
+        })
+        .then((categories) => {
+            const categorySelector = document.querySelector('.category-selector');
+            let categoryOptions = '';
+
+            // Ajouter le filtre "Tous" avec un identifiant de catégorie nul
+            categoryOptions += `<li data-id="">Tous</li>`;
+
+            categories.forEach((category) => {
+                categoryOptions += `<li data-id="${category.id}">${category.name}</li>`;
+            });
+
+            categorySelector.innerHTML = categoryOptions;
+            handleFilters();
+        })
+        .catch((error) => {
+            console.error('Erreur :', error);
+        });
+}
+
+function fetchWorks() {
+    fetch('http://localhost:5678/api/works')
+        .then((response) => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error('Erreur lors de la récupération des données');
+            }
+        })
+        .then((works) => {
+            listWorks = works;
+            updateGalleryWithFilteredWorks(null);
+            displayModal(works)
+        })
+        .catch((error) => {
+            console.error('Erreur :', error);
+        });
+}
+
+function checkAuth() {
+    const loginButton = document.querySelector('.connexion');
+    if (window.localStorage.getItem('authToken')) {
+        loginButton.innerHTML = `<a class="logout" href="index.html">Logout</a>`
+        const logout = document.querySelector('.logout');
+        const myProjects = document.querySelector('.my-projects');
+        logout.addEventListener('click', () => {
+            window.localStorage.removeItem('authToken');
+        })
+        const div = document.createElement('div');
+        div.className = 'edit'
+        div.innerHTML = '<i></i><span class="modify">Modifier</span>'
+        myProjects.appendChild(div);
+    } else {
+        loginButton.innerHTML = `<a href="login.html">Login</a>`
+    }
+    //loginButton.innerHTML = window.localStorage.getItem('authToken') ? `<a href="index.html">Logout</a>` : `<a href="login.html">Login</a>`
+}
+
+function displayModal(works) {
+    const modal = document.querySelector("#myModal");
+    const modify = document.querySelector(".modify");
+    const close = document.querySelector(".close");
+    const modalItemsContainer = document.querySelector(".modal-items");
+
+    modify.onclick = function () {
+        modal.style.display = "block";
+
+        // Ajoutez les images des works dans le conteneur .modal-items
+        works.forEach(work => {
+            const modalItem = document.createElement("div");
+            modalItem.className = "modalItem";
+
+            const img = document.createElement("img");
+            img.src = work.imageUrl;
+            img.alt = work.title;
+
+            const title = document.createElement("figcaption");
+            title.textContent = work.title;
+
+            modalItem.appendChild(img);
+            modalItem.appendChild(title);
+            modalItemsContainer.appendChild(modalItem);
+        });
+    };
+
+    close.onclick = function () {
+        modal.style.display = "none";
+    };
+
+    window.onclick = (event) => {
+        if (event.target == modal) modal.style.display = "none";
+    };
+}
+
+
+
+window.addEventListener('DOMContentLoaded', () => {
+    fetchCategories();
+    fetchWorks();
+    checkAuth();
+});
+
+
+function handleFilters() {
+    const categoryItems = document.querySelectorAll('.category-selector li');
+    categoryItems.forEach((categoryItem) => categoryItem.addEventListener('click', () => {
+        const selectedCategoryId = categoryItem.dataset.id;
+
+        if (currentSelectedCategoryId === selectedCategoryId) {
+            currentSelectedCategoryId = null;
+        } else {
+            currentSelectedCategoryId = selectedCategoryId;
+        }
+
+        // Supprime la classe 'selected' de tous les éléments de catégorie
+        categoryItems.forEach(item => item.classList.remove('selected'));
+
+        // Ajoute la classe 'selected' à l'élément de catégorie sélectionné, si une catégorie est sélectionnée
+        if (currentSelectedCategoryId !== null) {
+            categoryItem.classList.add('selected');
+        }
+
+        updateGalleryWithFilteredWorks(currentSelectedCategoryId);
+    }));
+}
+
+function updateGalleryWithFilteredWorks(categoryId) {
+    const filteredWorks = categoryId ? listWorks.filter(work => work.category.id == categoryId) : listWorks;
+    const gallery = document.querySelector('.gallery');
+    gallery.innerHTML = '';
+
+    filteredWorks.map(work => {
+        const figure = document.createElement('figure');
+        figure.dataset.id = work.id;
+
+        const img = document.createElement('img');
+        img.src = work.imageUrl;
+        img.alt = work.title;
+
+        const figcaption = document.createElement('figcaption');
+        figcaption.textContent = work.title;
+
+        figure.appendChild(img);
+        figure.appendChild(figcaption);
+
+        gallery.appendChild(figure);
+    });
+}
+
+function createWorkCards(works) {
+    const cardsContainer = document.createElement('div');
+    cardsContainer.className = 'gallery modal-items';
+
+    works.forEach(work => {
+        const figure = document.createElement('figure');
+        figure.dataset.id = work.id;
+
+        const img = document.createElement('img');
+        img.src = work.imageUrl;
+        img.alt = work.title;
+
+        const figcaption = document.createElement('figcaption');
+        figcaption.textContent = work.title;
+
+        figure.appendChild(img);
+        figure.appendChild(figcaption);
+        cardsContainer.appendChild(figure);
+    });
+
+    return cardsContainer;
+}
